a880ed9afa66e0b52e8aba827f614bca695b7046
Axzul777 <leandruino.dev@gmail.com>
2025-06-04 00:11:32 -0400

    other one

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { HonoRoutes } from 'api.js'
+
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+
+app.route('/', HonoRoutes)
+
 
 serve({
   fetch: app.fetch,
